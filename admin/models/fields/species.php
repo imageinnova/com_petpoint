90f8736e2be4afacafb2091bb11b142bad40105a
Max Schneider <max@imageinnova.com>
2015-05-29 17:56:53 +0000

    Replace field type for species with custom type species in back-end config

--- admin/models/fields/species.php
+++ admin/models/fields/species.php
@@ -1,31 +1,57 @@
 <?php
-defined('__JEXEC') or die();
-
-JFormHelper::LoadFieldClass('list');
-
-class JFormFieldSpecies extends JFormFieldList {
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_helloworld
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+JFormHelper::loadFieldClass('list');
+ 
+/**
+ * Species Form Field class for the Species component
+ *
+ * @since  0.0.1
+ */
+class JFormFieldSpecies extends JFormFieldList
+{
+	/**
+	 * The field type.
+	 *
+	 * @var         string
+	 */
 	protected $type = 'Species';
-	
-	protected function getOptions($published = false) {
-		$app = JFactory::getApplication();
-
-		$db = JFactory::getDBO();
+ 
+	/**
+	 * Method to get a list of options for a list input.
+	 *
+	 * @return  array  An array of JHtml options.
+	 */
+	protected function getOptions()
+	{
+		$db    = JFactory::getDBO();
 		$query = $db->getQuery(true);
-		$query->select('code,description');
-		$query->where('lang = "' . $app->get('language') . $published ? ' and published > 0' : '');
+		$query->select('species,description');
+		$query->where('published = 1');
 		$query->from('#__petpoint_species');
 		$db->setQuery((string) $query);
-		$results = $db->loadObjectList();
-		$options = array();
-		
-		if ($results):
-			foreach($results as $species):
-				$options[] = JHtml::_('select.option', $species->code, $species->description);
-			endforeach;
-		endif;
-		
+		$messages = $db->loadObjectList();
+		$options  = array();
+ 
+		if ($messages)
+		{
+			foreach ($messages as $message)
+			{
+				$options[] = JHtml::_('select.option', $message->species, $message->description);
+			}
+		}
+ 
 		$options = array_merge(parent::getOptions(), $options);
-		
+ 
 		return $options;
 	}
 }
